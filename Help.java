d0eeec49c68ec2383f85b7f33840c0c2dfc268b8
Elena140 <yastrebova.yelena@internet.ru>
2024-10-01 14:33:30 +0700

    Добавили разделы break и continue в справочную систему

--- Help.java
+++ Help.java
@@ -1,20 +1,24 @@
 class Help {
 	public static void main(String args[]) throws java.io.IOException {
-		char choice, ignore;
-		do {
+	  char choice, ignore;
+	  for(;;) {
+  		do {
 			System.out.println("Справка по операторам: ");
 			System.out.println(" 1. if ");
 			System.out.println(" 2. switch ");
 			System.out.println(" 3. for");
 			System.out.println(" 4. while");
 			System.out.println(" 5. do-while");
-			System.out.println(" Выберите номер раздела: ");
+			System.out.println(" 6. break");
+			System.out.println(" 7. continue\n");
+			System.out.println(" Выберите номер раздела(для завершения программы нажмите q): ");
 			choice = (char) System.in.read();
 			do{
 				ignore = (char) System.in.read();
 			} while (ignore != '\n');
-		} while (choice < '1' | choice > '5');
+		} while (choice < '1' | choice > '7' & choice != 'q');
 
+		if(choice == 'q') break;
 
 		System.out.println("\n");
 		switch(choice) {
@@ -47,6 +51,15 @@ class Help {
                                 System.out.println("  оператор");
                                 System.out.println("while (условие);");
 				break;
+			case '6':
+                                System.out.println("Оператор dreak:\n");
+                                System.out.println("  break; или break метка;");
+                                break;
+			case '7':
+                                System.out.println("Оператор dreak:\n");
+                                System.out.println("  continue; или continue меткаж");
+                                break;
+			}
 		}	
 	}
 }	
