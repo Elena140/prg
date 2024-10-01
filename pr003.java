7cb901fbac07e6fdcde2938d5b7d83b0e06f812b
Elena140 <yastrebova.yelena@internet.ru>
2024-10-01 13:44:30 +0700

    Добавили оператора continue

--- pr003.java
+++ pr003.java
@@ -265,6 +265,25 @@ stop2:			for(int j9=0; j9<5; j9++) {
 			}
 		}
 
-		//Использование оператора
+		//Использование оператора continue (прерывание текущей интерации)
+		System.out.println("\n");
+		for(int i9 = 0; i9<=100; i9++) {
+			if((i9%2) != 0) continue; //Переход к следующей интерации
+			System.out.println(i9 + " ");
+		}
+		System.out.println();
+
+		//continue с меткой
+		System.out.println("\n");
+		outerloop:
+		for(int i9=1; i9<10; i9++) {
+			 System.out.print("\nПроход внешнего цикла№" + i9 + ", внутренний цикл:");
+			  for(int j9=1; j9<10; j9++) {
+				  if(j9 == 5) continue outerloop; //продолжить внешний цикл
+				  System.out.print(j9);
+			}
+		}
+		System.out.println();
+
 	}
 }
