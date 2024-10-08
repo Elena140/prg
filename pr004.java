037305557dd3d901d9ff4f89b83449a337796c8f
Elena140 <yastrebova.yelena@internet.ru>
2024-10-08 13:00:25 +0700

    Возврат значений и использование параметров в методах

--- pr004.java
+++ pr004.java
@@ -6,6 +6,14 @@ class Avtomobil {
 	void rasst() {
 		System.out.println("Расстояние на полном баке: " + ((int)(v/rash * 100)));
 	}
+
+	int rasst2() {
+		return (int)(v/rash * 100);
+	}
+
+	double fuelNeeded(int km) { 
+		return ((double)km/100)*rash;	
+	}
 }
 class pr004 {
 	public static void main(String args[]) {
@@ -13,8 +21,8 @@ class pr004 {
 		Avtomobil lada = new Avtomobil();
 		Avtomobil porshe = new Avtomobil();
 	        int rasst, rasst2;
-
-		//Присваивание звначений внутренним переменным экземплярам класса
+	
+		//Присваивание значений внутренним переменным экземплярам класса
 		lada.pass = 4;
  		lada.v = 40;
 		lada.rash = 5.5;
@@ -23,14 +31,21 @@ class pr004 {
 		porshe.v = 100;
 		porshe.rash = 14.0;
 
-		
+		System.out.println("Для расстояния 10 км. нужно" + lada.fuelNeeded(10) + " литров бензина");
+		rasst=10;
+		System.out.println("Для расстояния 10 км. нужно" + lada.fuelNeeded(rasst) + " литров бензина");
+
 		//Расчет расстояния, которое lada  проедет на полном баке		
 		//rasst = (int) (lada.v/lada.rash * 100);
 		//rasst2 = (int) (porshe.v/porshe.rash * 100);
+
 		System.out.println("Для Lada: ");
 		lada.rasst();
+		rasst = lada.rasst2();
+		System.out.println("Результат вызова метода rasst2(): " + rasst);
 
 		System.out.println("Для Porshe: ");	
 		porshe.rasst(); 
+		System.out.println("Результат вызова метода rasst2(): " + porshe.rasst2() );
 	}
 }
