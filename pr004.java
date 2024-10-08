5cb7b9ad972c76ad0f82182ee68637558ad9df0d
Elena140 <yastrebova.yelena@internet.ru>
2024-10-08 14:54:12 +0700

    Использование конструктора в программе pr004

--- pr004.java
+++ pr004.java
@@ -3,6 +3,12 @@ class Avtomobil {
 	int v; //объем топливного бака в л.
 	double rash; //расход топлива в л. на 100км.
 
+	Avtomobil(int p, int V, double r) {
+		pass = p;
+		v = V;
+		rash = r;
+	}
+
 	void rasst() {
 		System.out.println("Расстояние на полном баке: " + ((int)(v/rash * 100)));
 	}
@@ -18,18 +24,19 @@ class Avtomobil {
 class pr004 {
 	public static void main(String args[]) {
 		//Создание экземпляра класса
-		Avtomobil lada = new Avtomobil();
-		Avtomobil porshe = new Avtomobil();
+		Avtomobil lada = new Avtomobil(4, 40, 5.6);
+		Avtomobil porshe = new Avtomobil(1, 100, 14,0);
 	        int rasst, rasst2;
 	
 		//Присваивание значений внутренним переменным экземплярам класса
-		lada.pass = 4;
+		/*lada.pass = 4;
  		lada.v = 40;
 		lada.rash = 5.5;
 
 		porshe.pass = 1;
 		porshe.v = 100;
 		porshe.rash = 14.0;
+		*/
 
 		System.out.println("Для расстояния 10 км. нужно" + lada.fuelNeeded(10) + " литров бензина");
 		rasst=10;
